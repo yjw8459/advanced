1f8d74da4e1b4ae02d1bdf3947c48815b1dd8676
yjw8459 <ryu01410@gmail.com>
2022-01-27 12:52:48 +0900

    김영한 스프링 고급 로그 추적기 V1 - 프로토타입 개발

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public TraceId(){
+        this.id = createId();
+        this.level = 0;
+    }
+
+    public TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId(){
+        return new TraceId(id, level + 1);
+    }
+
+    private TraceId createPreviousId(){
+        return new TraceId(id, level - 1 );
+    }
+
+    private boolean isFirstLevel(){
+        return level == 0;
+    }
+
+}
