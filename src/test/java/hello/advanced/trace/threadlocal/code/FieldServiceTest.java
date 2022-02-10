f2b743b20d81bbaab154833e65e62660e040f681
yjw8459 <ryu01410@gmail.com>
2022-02-10 23:41:38 +0900

    김영한 스프링 고급 - 6 쓰레드 로컬 (동시성 문제)

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/FieldServiceTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);    //쓰레드 A의 저장 시간보다 빠르게 쓰레드 B가 접근할 경우 동시성 문제 발생
+        threadB.start();
+        sleep(2000);
+
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+}
