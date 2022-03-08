3d7ee4192a57338339e3258cbca9b5361f677d1b
yjw8459 <ryu01410@gmail.com>
2022-03-09 03:39:41 +0900

    김영한 스프링 고급 - 1 템플릿 메서드 패턴 (디자인 패턴)

--- src/main/java/hello/advanced/app/v3/OrderControllerV3.java
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -19,7 +19,7 @@ public class OrderControllerV3 {
         TraceStatus status = null;
         try {
             status = trace.begin("OrderController.request()");
-            orderService.orderItem(itemId);
+            orderService.orderItem(itemId);//핵심 기능, 나머지는 부가 기능
             trace.end(status);
             return "ok";
         } catch (Exception e){
